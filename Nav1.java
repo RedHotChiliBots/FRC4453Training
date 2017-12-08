a52aeeadce4ef5f434b169f8a0aa5c95a59d2c46
connerebbinghaus <connerebbinghaus@users.noreply.github.com>
2017-12-07 19:00:27 -0500

    Create Nav1.java
    Boilerplate code.

---
+++ Nav1.java
@@ -0,0 +1,10 @@
+
+class Nav1 {
+    static public float calcDistance(float X1, float Y1, float X2, float Y2) {
+        // Code here
+    }
+    
+    static public float calcAngle(float X1, float Y1, float X2, float Y2, float angle) {
+        // Code here
+    }
+}
